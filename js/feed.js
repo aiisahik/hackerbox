bacb40cbc56d464c1678cb82563c9224b74ecf5f
Michelle <michesun@gmail.com>
2012-08-12 03:26:51 +0800

    passing

--- js/feed.js
+++ js/feed.js
@@ -18,38 +18,38 @@ $(function(){
    });
 
 // -- VIEWS
-   // window.ParticipantView = Backbone.View.extend({
-   //    template: _.template($("#participant-template").html()),
-   //    tagName: 'tr',
-   //    // events: {
-   //    //    'click .thumbnail': 'toNode',
-   //    // },
-   //    initialize: function() {
-   //       _.bindAll(this, 'render');
-   //       this.model.bind('reset', this.render);
-   //    },
-   //    render: function() {
-   //       $(this.el).empty().append(this.template(this.model.toJSON()));
-   //       return this;
-   //    },
-   // });
+   window.FeedView = Backbone.View.extend({
+      template: _.template($("#feed-template").html()),
 
-   // window.ParticipantsView = Backbone.View.extend({
-   //    tagName: 'tbody',
-   //    initialize: function() {
-   //       _.bindAll(this, 'render');
-   //       this.collection.bind('reset', this.render);
-   //    },
-   //    render: function() {
-   //       _.each(this.collection.models, function(participant){
-   //          var participantView = new ParticipantView({
-   //             model: participant
-   //          });
-   //          $(this.el).append(participantView.render().el);
-   //       }, this);
-   //       return this;
-   //    }
-   // });
+      // events: {
+      //    'click .thumbnail': 'toNode',
+      // },
+      initialize: function() {
+         _.bindAll(this, 'render');
+         this.model.bind('reset', this.render);
+      },
+      render: function() {
+         $(this.el).empty().append(this.template(this.model.toJSON()));
+         return this;
+      },
+   });
+
+   window.FeedsView = Backbone.View.extend({
+      tagName: 'tbody',
+      initialize: function() {
+         _.bindAll(this, 'render');
+         this.collection.bind('reset', this.render);
+      },
+      render: function() {
+         _.each(this.collection.models, function(participant){
+            var participantView = new ParticipantView({
+               model: participant
+            });
+            $(this.el).append(participantView.render().el);
+         }, this);
+         return this;
+      }
+   });
 
    var AppRouter = Backbone.Router.extend({
       routes: {
@@ -72,7 +72,6 @@ $(function(){
          window.participants = new Participants();
          window.participants.fetch({
 
-          
           success: function(collection) {
               var container = $('#participant-container');
               var main = $('#main-tab-content');
@@ -93,98 +92,33 @@ $(function(){
                      });
                  main.append(rendered);
 
-
-                 
               });
-              self.getFeed("CTeaSW3801");
-           
-              // window.participantsView = new ParticipantsView({
-              //    collection: window.participants
-              // });
-              // $("#participants").after(window.participantsView.render().el);
-              // self.getFeed();
+              self.getFeed(window.participants.first());
+        
            },
            error: function(collection, error) {
-             // The collection could not be retrieved.
            }
          });
       },
+      getFeed: function(participant){
 
-      getFeed: function(participantId){
+        // window.feed = new window.Emails();
+        var Email = Parse.Object.extend("Email");
+        window.query = new Parse.Query(Email);
+        window.query.equalTo("participant",participant);
 
-
-          window.feed = new window.Emails();
-
-         _.each(window.participants.models, function(participant){
-            // window.email = new Emails();
-            var Email = Parse.Object.extend("Email");
-            window.email.query = new Parse.Query(Email);
-            window.email.query.equalTo("toEmail","jie.z.zhou@gmail.com");
-            window.email.fetch();
-
-            // window.feedCollection = query.collection();
-            // email.find({
-            //   success: function(results) {
-            //     for (i=0; i<results.length; i++){
-            //       results[i]
-            //     };
-            //     alert("Successfully retrieved " + results.length + " scores.");
-            //   },
-            //   error: function(error) {
-            //     alert("Error: " + error.code + " " + error.message);
-            //   }
-// // Create our collection of Todos
-// this.todos = new TodoList;
-
-// // Setup the query for the collection to look for todos from the current user
-// this.todos.query = new Parse.Query(Todo);
-// this.todos.query.equalTo("user", Parse.User.current());
-
-
-
-            //var EmailObject = Parse.Object.extend("Email");
-            window.feed.query = new Parse.Query(Email);
-            window.feed.query.equalTo("participant",participant);
-            //window.feed.query.equalTo("fromEmail","survey@youxresearch.com");
-
-            window.feed.fetch({
-              success: function(res) {
-                console.log(res);
-
-              }, 
-              error: function(res){
-                console.log(res);
-              }
-            });
-            //console.warn(window.feedCollection);
-            // window.feed = new Emails();
-
-            // window.feed.equalTo("participant",participant);
-            // window.feed.fetch({
-            //    success: function(collection) {
-            //       collection.each(function(object){
-            //          console.warn(object);
-            //       });
-
-            //       // var query = new Parse.Query(GameScore);
-            //       // query.equalTo("playerEmail", "dstemkoski@example.com");
-            //       // query.first({
-            //       // success: function(object) {
-            //       //   // Successfully retrieved the object.
-            //       // },
-            //       //   error: function(error) {
-            //       //     alert("Error: " + error.code + " " + error.message);
-            //       // }
-            //    },
-
-            //     error: function(collection, error) {
-            //  // The collection could not be retrieved.
-            //   }
-            //});
-         // });
+        // window.query.equalTo("toEmail","jie.z.zhou@gmail.com");
+        window.feed = window.query.collection();
+        window.feed.fetch({
+          success: function(res) {
+            console.log(res);
+          }, 
+          error: function(res){
+            console.log(res);
+          }
+        });
 
       }
-
    });
 
    $(function() {
