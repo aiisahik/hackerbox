762b0fc202e92fff96bb5b6683587f3525140860
aiisahik <jie.z.zhou@gmail.com>
2012-08-11 23:06:10 -0700

    send emails

--- appengine.py
+++ appengine.py
@@ -9,30 +9,10 @@ import urlparse
 import hashlib, base64
 import sendgrid
 import parsepy
+import logging
 
 parsepy.APPLICATION_ID = "IjTmpLYQk8sI3Vhhv2IbCprhrZ4pCnpy9yELySQ8"
 parsepy.MASTER_KEY = "iGW730DEr0h5VA4110jCwQf0TJjnIvyNIWRXemT8"
-
-
-def sendemail(participant):
-## 2. Send email to get recipient input 
-	s = sendgrid.Sendgrid('jinxdabinx', 'sendgrid', secure=True) 
-	# include user and password
-
-	# unique_email = "%s%s%d%d@youxresearch.com" %(coname,survey,surveyid,userid)
-	message = sendgrid.Message("survey@youxresearch.com", "subject", "Hello %name%, your code is %code%", "<b>Hello %name%, your code is %code%</b>")
-
-	for p in participant:
-		message.add_to(
-			    {
-			    	p.email : {'%name%': p.firstName},
-			    }
-		    )
-	# change to bcc. 
-		s.web.send(message)
-# username, password
-
-
 
 
 class HomeHandler(webapp2.RequestHandler):
@@ -48,7 +28,6 @@ class SetupHandler(webapp2.RequestHandler):
 
 class SetupPostHandler(webapp2.RequestHandler):
 	def post(self):
-
 		participant = parsepy.ParseObject("Participant")
 		participant.email = self.request.get('email_address')
 		participant.firstName = self.request.get('first_name')
@@ -56,21 +35,56 @@ class SetupPostHandler(webapp2.RequestHandler):
 		participant.phoneNumber = self.request.get('phone_number')
 		participant.save()
 
-class GenEmailHandler(webapp2.RequestHandler):
+class EmailHandler(webapp2.RequestHandler):
 	# a page for user email / first/last name / phone
     def get(self):
-		path = os.path.join(os.path.dirname(__file__), 'gen_email.html')
-		participant = parsepy.ParseQuery("Participant")
-		# does it return a list of dictionary
-		# call a function that send out email
+		path = os.path.join(os.path.dirname(__file__), 'email.html')
+		# participant = parsepy.ParseQuery("Participant")
 		self.response.out.write(template.render(path,{}))
+		logging.info('sending email:')
 
 	# def post(self):
 	# 	pass
+
+class EmailSubmitHandler(webapp2.RequestHandler):
+    def post(self):
+		s = sendgrid.Sendgrid('jinxdabinx', 'sendgrid', secure=True) 
+		# include user and password
+		q = parsepy.ParseQuery("Participant")
+		Participants = q.fetch()
+		logging.info('sending email:')
+
+		for p in Participants:
+			if self.request.get('check_'+p.email) == "on" :
+
+				emailObject = parsepy.ParseObject("Email")
+				emailObject.participant = p
+				emailObject.subject = self.request.get('subject')
+				emailObject.body = self.request.get('body')
+				plaintext = emailObject.body
+				html = emailObject.body.replace("/n","<br>")
+				emailObject.save()
+
+				logging.info('sending email: ' + plaintext)
+
+				message = sendgrid.Message("survey@youxresearch.com", emailObject.subject, 
+					plaintext , html)
+
+				message.add_to(
+					    {
+					    	p.email : {'%firstname%': p.firstName},
+					    }
+				    )
+				# change to bcc. 
+				s.web.send(message)
+
+			# username, password
+
 
 app = webapp2.WSGIApplication([
     webapp2.Route(r'/', handler=HomeHandler, name='home'),
     webapp2.Route(r'/setup', handler=SetupHandler, name='setup'),
     webapp2.Route(r'/setup/submit', handler=SetupPostHandler, name='setup_submit'),
-    webapp2.Route(r'/gen_email', handler=GenEmailHandler, name='gen_email') ])
+    webapp2.Route(r'/email', handler=EmailHandler, name='email'),
+    webapp2.Route(r'/email/submit', handler=EmailSubmitHandler, name='email_submit') ])
 
--- js/app.js
+++ js/app.js
@@ -1,69 +1,91 @@
 $(function(){
 
-   // Brand Model
+
+   Parse.initialize("IjTmpLYQk8sI3Vhhv2IbCprhrZ4pCnpy9yELySQ8", "I9JnUSqpP9bcqhpwIialhWZGcNtXTCTnKzqZqWwq");
+
+   // Participant Model
    // ----------
-   window.Brand = Backbone.Model.extend();
-   window.Brands = Backbone.Collection.extend({
-      model: Brand
+   //window.Participant = Backbone.Model.extend();
+
+   Participant = Parse.Object.extend("Participant");
+   window.Participants = Parse.Collection.extend({
+      model: Participant
    });
 
 
-//-- VIEWS
+// -- VIEWS
 
-   // Brand VIEW
+
+  // PERSON VIEW
    // ----------
-   window.BrandView = Backbone.View.extend({
-      template: _.template($("#brand-template").html()),
-      tagName: 'div',
-      className: "span10",
-
-      events: {
-         'click .thumbnail': 'toNode',
-      },
+   window.ParticipantView = Backbone.View.extend({
+      template: _.template($("#participant-template").html()),
+      tagName: 'tr',
+      // events: {
+      //    'click .thumbnail': 'toNode',
+      // },
       initialize: function() {
          _.bindAll(this, 'render');
          this.model.bind('reset', this.render);
       },
       render: function() {
-
-         // _.each(this.model.images, function(image){
-         //    $(this.el).append('<img href="'+image+'" />');
-         // }
          $(this.el).empty().append(this.template(this.model.toJSON()));
          return this;
       },
-      toNode: function(){
-         ///treeNodesView.update(this.model.get('id'));
-      }
    });
 
-   // Brands VIEW
-   // ----------
-
-   window.BrandsView = Backbone.View.extend({
-      tagName: 'div',
-      className: "row-fluid",
-      
+//    Brand VIEW
+//    ----------
+   window.ParticipantsView = Backbone.View.extend({
+      tagName: 'tbody',
+      // events: {
+      //    'click .thumbnail': 'toNode',
+      // },
       initialize: function() {
          _.bindAll(this, 'render');
          this.collection.bind('reset', this.render);
       },
       render: function() {
-         $(this.el).empty();
-         _.each(this.collection.models, function(brand){
-            var brandView = new BrandView({
-               model: brand
+         _.each(this.collection.models, function(participant){
+            var participantView = new ParticipantView({
+               model: participant
             });
-            $(this.el).append(brandView.render().el);
+            $(this.el).append(participantView.render().el);
          }, this);
-         //$('#persons').empty().append($(this.el));
-         //-- resize node to below 200 px --//
-         
+
+         //$("#participants").empty().append(this.el);
          return this;
       }
-
-
    });
+
+
+   // Brands VIEW
+   // ----------
+
+   // window.BrandsView = Backbone.View.extend({
+   //    tagName: 'div',
+   //    className: "row-fluid",
+      
+   //    initialize: function() {
+   //       _.bindAll(this, 'render');
+   //       this.collection.bind('reset', this.render);
+   //    },
+   //    render: function() {
+   //       $(this.el).empty();
+   //       _.each(this.collection.models, function(brand){
+   //          var brandView = new BrandView({
+   //             model: brand
+   //          });
+   //          $(this.el).append(brandView.render().el);
+   //       }, this);
+   //       //$('#persons').empty().append($(this.el));
+   //       //-- resize node to below 200 px --//
+         
+   //       return this;
+   //    }
+
+
+   // });
 
    var AppRouter = Backbone.Router.extend({
       routes: {
@@ -75,69 +97,69 @@ $(function(){
 
       home: function() {
          this.getData();
-         
-
       },
 
       render: function(){
-
+         
       },
 
       getData: function(){
-         $.ajax({
-            url: "json/test1.json",
-            success: function(data) {
-
-               window.resultBrands = new Brands();
-               window.resultBrands.add(data.results);
+         window.participants = new Participants();
+         window.participants.fetch({
+           success: function(collection) {
+               collection.each(function(object) {
+                  console.warn(object);
+            // $(this.el).append(personView.render().el);
                
-               window.queryBrand = new Brands();
-               window.queryBrand.add(data.query);
+             });
 
-               window.resultsView = new BrandsView({
-                  collection: window.resultBrands
-               });
+            window.participantsView = new ParticipantsView({
+               collection: window.participants
+            });
 
-               window.queryView = new BrandsView({
-                  collection: window.queryBrand
-               });
-
-               $("#results").append(window.resultsView.render().el);
-
-            }
-         // .done(function() { 
-         //       return null;
-         //    });
+            $("#participants").after(window.participantsView.render().el);
+            $("#check_all").click(function() {
+               $("#participant_table").find("input").prop("checked",true)
+            });
+             
+           },
+           error: function(collection, error) {
+             // The collection could not be retrieved.
+           }
          });
+
       },
 
-      bindSearch: function() {
 
-         // $("#search").find('li').height(150);
-         // $("#persons").find('.thumbnail').height(150);
 
-         // var firstNames = window.personsList.pluck('firstName');   
-         // var lastNames = window.personsList.pluck('lastName');
-         // window.names = [];
-         // for(var i=0; i<lastNames.length; ++i) {
-         //    window.names.push(firstNames[i] + " " + lastNames[i]);
-         // }
-         $("#search").autocomplete({source: window.names});
 
-         $("#search").keypress(function(event) {
-            if ( event.which == 13 ) {
-               if ($("#searchbox").val() == "") {
-                   $("#results").append(window.resultsView.render().el);
-               } else {
-                  event.preventDefault();
-                  var search_string = $("#searchbox").val();
-                  // if (person_index >=0 ) {
-                  //    window.personsView.render_person(window.personsList.at(person_index));
-                  // }
-               }
-            }
-         });
-      }
+      // bindSearch: function() {
+
+      //    // $("#search").find('li').height(150);
+      //    // $("#persons").find('.thumbnail').height(150);
+
+      //    // var firstNames = window.personsList.pluck('firstName');   
+      //    // var lastNames = window.personsList.pluck('lastName');
+      //    // window.names = [];
+      //    // for(var i=0; i<lastNames.length; ++i) {
+      //    //    window.names.push(firstNames[i] + " " + lastNames[i]);
+      //    // }
+      //    $("#search").autocomplete({source: window.names});
+
+      //    $("#search").keypress(function(event) {
+      //       if ( event.which == 13 ) {
+      //          if ($("#searchbox").val() == "") {
+      //              $("#results").append(window.resultsView.render().el);
+      //          } else {
+      //             event.preventDefault();
+      //             var search_string = $("#searchbox").val();
+      //             // if (person_index >=0 ) {
+      //             //    window.personsView.render_person(window.personsList.at(person_index));
+      //             // }
+      //          }
+      //       }
+      //    });
+      // }
 
 
    });
