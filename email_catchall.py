41b13795e67b2ef4e740032546645fa214daece5
aiisahik <jie.z.zhou@gmail.com>
2012-08-11 17:25:23 -0700

    fixed email problem

--- email_catchall.py
+++ email_catchall.py
@@ -1,18 +1,24 @@
+import wsgiref.handlers
+import cgi
 import logging, email
-from google.appengine.ext import webapp 
+from google.appengine.ext import webapp
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+# message = mail.InboundEmailMessage(self.request.body)
+#          logging.info("Received a message from: " + mail_message.sender)
 
 class LogSenderHandler(InboundMailHandler):
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
+        logging.info("Received a message from: " + mail_message.to)
+        logging.info("Received a message from: " + mail_message.subject)
+        logging.info("Received a message from: " + mail_message.body)
 
+     #    plaintext_bodies = mail_message.bodies('text/plain')
+    	# html_bodies = mail_message.bodies('text/html')
 
-        plaintext_bodies = mail_message.bodies('text/plain')
-    	html_bodies = mail_message.bodies('text/html')
+    	# for content_type, body in html_bodies:
+     #    	decoded_html = body.decode()
 
-    	for content_type, body in html_bodies:
-        	decoded_html = body.decode()
-
-
-        	
+app = webapp.WSGIApplication([LogSenderHandler.mapping()], debug=True)
